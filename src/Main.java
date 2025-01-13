1684ccf27463d237c7661ca3db8824c131853bc1
Miroslava Vesnina <miroslavavesnina@gmail.com>
2025-01-13 18:54:46 +0300

    add printscreen modif main

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         int eaters = 5; // сколько людей будут есть
 
         int water = 3001; // миллилитров воды
-        int potatoes = 8; // картофелин
+        int potatoes = 9; // картофелин
         int chicken = 7; // куриных бёдер
-        int spices = 11; // ложек специй
+        int spices = 8; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров(а) воды");
